a9a52c2aa1c60d170cb9862745a2961495998bbf
SML <smlbiobot@gmail.com>
2021-05-21 09:15:49 +0800

    Parse file with BeautifulSoup and output with custom beautified indents

--- twprefix.py
+++ twprefix.py
@@ -8,9 +8,26 @@ on the web that does not have a prefix
 """
 import argparse
 import os
+import re
+
 from bs4 import BeautifulSoup
 
-def process(filepath=None):
+
+def prettify_with_space(
+        s: BeautifulSoup,
+        encoding=None,
+        formatter="minimal",
+        indent: int = 2
+):
+    """
+    Re-indent the BS4 output
+    https://stackoverflow.com/questions/15509397/custom-indent-width-for-beautifulsoup-prettify
+    """
+    r = re.compile(r'^(\s*)', re.MULTILINE)
+    return r.sub(r'\1' * indent, s.prettify(encoding, formatter))
+
+
+def process(filepath=None, prefix='tw-', indent=2):
     """
     Process the filepath
     :param filepath: source file. Use file from tests if None
@@ -19,7 +36,35 @@ def process(filepath=None):
     with open(filepath) as fp:
         soup = BeautifulSoup(fp, 'lxml-xml')
 
-    print(soup)
+    tags_with_class = soup.find_all(attrs={'class': True})
+
+    for tag in tags_with_class:
+        class_names = tag['class'].split(' ')
+        tag['class'] = ' '.join([f"{prefix}{c}" for c in class_names])
+
+    tags_with__class = soup.find_all(attrs={':class': True})
+    for tag in tags_with__class:
+        value = tag[':class']
+        replacements = []
+        matches = re.findall(r'\'(.+?)\'', value)
+        for m in matches:
+            src = m
+            if ' ' in m:
+                class_names = m.split(' ')
+                dst = ' '.join([f"{prefix}{c}" for c in class_names])
+            else:
+                dst = f"{prefix}{src}"
+            replacements.append(
+                dict(src=src, dst=dst)
+            )
+
+        if replacements:
+            for r in replacements:
+                value = value.replace(r['src'], r['dst'])
+
+        tag[':class'] = value
+
+    print(prettify_with_space(soup, indent=indent))
 
 
 def main():
@@ -29,6 +74,7 @@ def main():
         help='File to process',
         default='./tests/PageHeadings.vue',
     )
+    # TODO: add option to output rendered template to file
     ap.add_argument(
         '-o',
         '--out',
